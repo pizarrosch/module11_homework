fab77fd2ab5717c154df02db99e7b0b3b8419bc7
pizarrosch <bayernsch89@gmail.com>
2022-12-16 22:34:46 +0100

    Task 5

---
+++ Task 5.js
@@ -0,0 +1,9 @@
+const result = (x, n) => console.log(x ** n);
+
+result(7, 5);
+
+
+// как альтернативу можно использовать следующий вариант
+
+const anotherResult = (x, n) => console.log(Math.pow(x, n));
+anotherResult(7, 5);
